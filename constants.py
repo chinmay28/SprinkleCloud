419e527d5c7d1ed27451fda948765358cd2fac32
Chinmay Manjunath <chinmay@Chinmays-MacBook-Pro.local>
2019-04-07 18:04:08 -0700

    adding test files

---
+++ constants.py
@@ -0,0 +1,12 @@
+
+
+# CloudWriter names
+BASE_CLOUD = 'base'
+DROPBOX = 'dropbox'
+GOOGLE_DRIVE = 'gdrive'
+ONEDRIVE = 'onedrive'
+
+# Get operations
+METADATA = 'meta'
+CONTENT = 'content'
+DOWNLOAD = 'download'
